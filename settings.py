6e77fd46523df37a6402dbcb6fa359a13c0a529f
Chengfei Wang <czw0078@auburn.edu>
2021-06-25 01:01:39 -0500

    add node and road class

--- settings.py
+++ settings.py
@@ -19,7 +19,7 @@ th = 0.5*animation_window_height
 sw = animation_window_width/world_width_m
 sh = animation_window_height/world_height_m
 
-total_ticks = 500 # 3600 = 1 hour
+total_ticks = 50 # 3600 = 1 hour
 
 def convert_to_window(x, y):
     w = int(x*sw + tw)
@@ -45,17 +45,24 @@ def config_canvas(canvas):
     canvas.configure(bg="black")
     canvas.pack(fill="both", expand=True)
 
-class Vehicle():
+class Vehicle:
 
     def __init__(self, canvas, x, y):
-        self.canvas = canvas
+        # world
         self.x = x
         self.y = y
         self.delta_x = 5
         self.delta_y = 5
+        # screen
         self.w = 0
         self.h = 0
+        self.canvas = canvas
         self.sprite = self._init_sprite(self.canvas)
+        # road
+        self.v = 7
+        self.a = 0
+        self.s = 0
+        self.road = None
 
     def _init_sprite(self, canvas):
         self.w, self.h = convert_to_window(self.x, self.y)
@@ -81,6 +88,40 @@ class Vehicle():
             self.delta_x = - self.delta_x
         if self.y < - world_height_m/2 + 1 or self.y > world_height_m/2 - 1:
             self.delta_y = - self.delta_y
+
+class Road:
+    def __init__(self, start, end):
+        self.x1 = start.x
+        self.y1 = start.y
+        self.x2 = end.x
+        self.y2 = end.y
+        self.l = ((self.x1 - self.x2)**2 + (self.y1 - self.y2)**2)**0.5
+        self.v_max = 25
+        self.vehicle_queue = None
+
+    def enter(self, s):
+        pass
+
+    def exit(self):
+        pass
+        # return new road
+
+    def neighbors(self):
+        pass
+        # return d, v
+
+    def navigate(self):
+        pass
+        # return dx, dy
+
+class Node:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.inflow = set()
+        self.outflow = set()
+
+
 
 
 
